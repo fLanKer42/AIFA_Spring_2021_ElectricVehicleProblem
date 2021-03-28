3cb077e14a84153f652e658f3fdfc5df92b977f6
fLanKer42 <dhruv.rai2001@gmail.com>
2021-03-28 19:35:42 +0530

    input method added

--- A_star_heuristic.py
+++ A_star_heuristic.py
@@ -113,78 +113,54 @@ def path_plot(car_s: car, city_list):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-    
-
-
-    
-    
-
-
 if __name__ == '__main__':
+    n = int(input('Enter number of cars'))
+    m = int(input('Enter number of cities'))
     city_list = list()
     car_list = list()
-    ##################################################################
-    ###################### TEST DATA #################################
-    ##################################################################
-    e12 = 12
-    e13 = 16
-    e23 = 17
+    dist_matrix = [[None]*(m)]*m
 
-    B1= 100
-    M1= 100
-    C1= 1
-    S1= 1
-    D1= 1
-
-    B2= 100
-    M2= 100
-    C2= 1
-    S2= 1
-    D2= 1
-
-    B3= 100
-    M3= 100
-    C3= 1
-    S3= 1
-    D3= 1
-    V1 = city(0,[0,e12,e13])
-    V2 = city(1,[e12,0,e23])
-    V3 = city(2,[e13,e23,0])
-
-    c1 = car(V1,V2,B1,D1,M1,S1,C1)
-    c2 = car(V2,V3,B2,D2,M2,S2,C2)
-    c3 = car(V3,V1,B3,D3,M3,S3,C3)
-
-    city_list = [V1,V2,V3]
-    car_list = [c1,c2,c3]
-    ##################################################################
+    for i in range(m):
+        for j in range(i,m):
+            if i == j:
+                dist_matrix[i][j] = 0
+                dist_matrix[j][i] = 0
+            else:
+                dist_matrix[i][j] = float(input('enter distance between city '+str(i)+' and '+str(j)+' : '))
+                dist_matrix[j][i] = dist_matrix[i][j]
+    for i in range(m):
+        v = city(i,dist_matrix[i])
+        city_list.append(v)
+    for i in range(n):
+        sr = int(input('enter the source city of car number '+str(i)))
+        de = int(input('enter the destination city of car number '+ str(i)))
+        b = float(input('enter the initial charge'))
+        d = float(input('enter the discharge rate'))
+        max_b = float(input('enter the maximum charge capacity'))
+        crg_r = float(input('enter the charge rate of the car'))
+        avg_s = float(input('enter the average speed of the car'))
+        car_s = car(city_list[sr],city_list[de],b,d,max_b,avg_s,crg_r)
+        car_list.append(car_s)
     for i in range(len(car_list)):
         car_list[i] = path_plot(car_list[i],city_list)
 
+    print('The following are the paths of each car')
+    for i in range(len(car_list)):
+        print(i,':',car_list[i].path)
+        
 
-    for i in car_list:
-        print(i.path)
 
-    ###################
-    #todo
-    ###################
+
+
+
+
+
+    
+    
     
 
 
-
+    
+    
 
 
